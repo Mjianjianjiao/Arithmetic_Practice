51c14cbe9b3d48c2068ea593d70bae13a9c719af
Centos6.5 <wens@localhost.localdomain>
2018-09-28 15:38:37 -0700

    move the odd number in front of the enev number in an Array

--- ReoderArray/ReoderArray.c
+++ ReoderArray/ReoderArray.c
@@ -22,6 +22,37 @@ void ReoderArray(int* a,int n)
             end--;
             }  
     }
+}
+void ReoderArray2(int *a,int n)
+{
+    int start=0;
+    int end=n-1;
+    int t=0;
+    if(a==NULL&&n<=0)
+        return ;
+    while(start<end)
+    {
+        while(start<end)
+        {
+            if(a[start]%2!=0)
+                start++;
+            else 
+                break;
+        }
+        while(start<end)
+        {
+            if(a[end]%2==0)
+                end--;
+            else
+                break;
+        }
+        if(start<end)
+        {
+            t=a[start];
+            a[start]=a[end];
+            a[end]=t;
+        }
+    }
 
 }
 void Print(int *a,int n)
@@ -40,7 +71,8 @@ int main()
     int a[]={1,2,3,4,5,6,7,8,9,10};
     int n=sizeof(a)/sizeof(a[0]);
     int i=0;
-    ReoderArray(a,n);
+   // ReoderArray(a,n);
+    ReoderArray2(a,n);
     Print(a,n);
     return 0;
 }
